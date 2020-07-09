c8a5bc2a90fc0708e369c545fac515a2e01abe4d
Steven McAllister <steved1mac@gmail.com>
2020-07-09 15:42:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 final results = data.each_with_object({}) do |(key, value), final_array
     value.each do |inner_key, name|
-      names.each do |name|
+      name.each do |name|
         if !final_array[name]
           final_array[name] = {}
         end
